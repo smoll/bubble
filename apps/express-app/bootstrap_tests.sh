82ab61ea9ba2a070e1e632845f54ec10c617ff50
smoll <smollah@theorchard.com>
2015-06-22 12:53:59 -0400

    Run tests in a different port & create bootstrap script

---
+++ apps/express-app/bootstrap_tests.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Run tests in a different server process, via `npm test`
+
+set -e
+
+# Do setup
+npm install
+export PORT=3001
+npm start &
+
+printf "Starting webserver"
+until $(curl --output /dev/null --silent --head --fail http://127.0.0.1:$PORT); do
+  printf '.'
+  sleep 0.01
+done # Block until server is up, http://stackoverflow.com/a/21189440/3456726
+printf "\n"
+
+# Do tests
+echo Running tests on port $PORT...
+cd tests/e2e
+bundle install
+bundle exec cucumber
+
+# Clean up on exit, http://stackoverflow.com/a/22644006/3456726
+trap "exit" INT TERM
+trap "kill 0" EXIT
